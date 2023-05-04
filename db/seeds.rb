4494e5cb40ad8137bef4e5bcbdcdcea3aaa5ac47
alexis <dupray_a@hotmail.fr>
2023-05-04 23:39:58 +0200

    airbnbdog

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+dog = Dog.create(name :"Luna")
+Dog.create(name :"Luno")
+Dog.create(name :"Luni")
+Dog.create(name :"Lune")
+
+Dogsitter.create(name: "Alexis")
+Dogsitter.create(name: "Marie")
+Dogsitter.create(name: "samuel")
+
+City.create(name: "Mantes-la-Jolie")
+City.create(name: "Rosny-sur-seine")
+
